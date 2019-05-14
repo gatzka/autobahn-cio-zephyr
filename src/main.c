6b2057f13edc80b6b0a6ed4c529bbef7645f454b
Stephan Gatzka <stephan.gatzka@hbm.com>
2019-05-14 13:17:09 +0200

    Make simple server socket application.

--- src/main.c
+++ src/main.c
@@ -12,60 +12,61 @@
 
 #include "cio_error_code.h"
 #include "cio_eventloop.h"
-#include "cio_timer.h"
+#include "cio_server_socket.h"
+#include "cio_util.h"
 
 static struct cio_eventloop loop;
-static const uint64_t FIVE_S = 5000000000;
-static uint_fast8_t expirations = 0;
-static const uint_fast8_t MAX_EXPIRATIONS = 5;
 
-static void handle_timeout(struct cio_timer *timer, void *handler_context, enum cio_error err)
+static const uint64_t close_timeout_ns = UINT64_C(1) * UINT64_C(1000) * UINT64_C(1000) * UINT64_C(1000);
+enum {BUFFER_SIZE = 100};
+
+struct echo_client {
+	struct cio_socket socket;
+	uint8_t buffer[BUFFER_SIZE];
+	struct cio_write_buffer wb;
+	struct cio_write_buffer wbh;
+	struct cio_read_buffer rb;
+};
+
+static struct cio_socket *alloc_echo_client(void)
 {
-	(void)handler_context;
-	if (err == CIO_SUCCESS) {
-		printk("timer expired!\n");
-		if (expirations++ < MAX_EXPIRATIONS) {
-			if (cio_timer_expires_from_now(timer, FIVE_S, handle_timeout, NULL) != CIO_SUCCESS) {
-				printk("arming timer failed!\n");
-			}
-		} else {
-			cio_timer_close(timer);
-			cio_eventloop_cancel(&loop);
-		}
-	} else {
-		printk("timer error!\n");
+	struct echo_client *client = malloc(sizeof(*client));
+	if (cio_unlikely(client == NULL)) {
+		return NULL;
 	}
+
+	return &client->socket;
+}
+
+static void free_echo_client(struct cio_socket *socket)
+{
+	struct echo_client *client = cio_container_of(socket, struct echo_client, socket);
+	free(client);
 }
 
 int main(void)
 {
-	int ret = 0;
 	enum cio_error err = cio_eventloop_init(&loop);
 	if (err != CIO_SUCCESS) {
 		return -1;
 	}
 
-	struct cio_timer timer;
-	err = cio_timer_init(&timer, &loop, NULL);
-	if (err != CIO_SUCCESS) {
-		ret = -1;
-		goto destroy_loop;
-	}
 
-	if (cio_timer_expires_from_now(&timer, FIVE_S, handle_timeout, NULL) != CIO_SUCCESS) {
-		printk("arming timer failed!\n");
-		ret = -1;
-		goto destroy_loop;
+	struct cio_server_socket ss;
+	err = cio_server_socket_init(&ss, &loop, 5, alloc_echo_client, free_echo_client, close_timeout_ns, NULL);
+	if (err != CIO_SUCCESS) {
+		printk("error in cio_eventloop_run!\n");
+		goto destroy_eventloop;
 	}
 
 	err = cio_eventloop_run(&loop);
 	if (err != CIO_SUCCESS) {
 		printk("error in cio_eventloop_run!\n");
+		goto destroy_eventloop;
 	}
 
-destroy_loop:
+destroy_eventloop:
 	cio_eventloop_destroy(&loop);
 
-	return ret;
-
+	return 0;
 }
